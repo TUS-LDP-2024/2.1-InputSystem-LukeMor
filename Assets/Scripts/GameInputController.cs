d8fc60c5a152d62583ed170f7a90acba5941e270
Beanie <K00281273@student.tus.ie>
2024-09-17 10:39:31 +0100

    Movement System Added

--- Assets/Scripts/GameInputController.cs
+++ Assets/Scripts/GameInputController.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameInputController : MonoBehaviour
 {
+    public Rigidbody rb;
+    public float MoveSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    public void OnMove(InputValue value)
+    {
+        rb.velocity = value.Get<Vector2>() * MoveSpeed;
     }
 
     // Update is called once per frame
